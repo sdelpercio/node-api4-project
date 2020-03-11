e024681d3a0510eb0b9d72b36e392dd92af78bbd
Shawn DelPercio <sdelperc@gmail.com>
2020-03-11 15:21:51 -0400

    started up server; created logger middleware

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
 const server = express();
+server.use(logger);
+server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	const method = req.method;
+	const path = req.originalUrl;
+	console.log(`Request logged: ${method} to ${path}`);
+	next();
+}
 
 module.exports = server;
